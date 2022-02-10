5e01085aad07887be667ace051e1838fa1890264
iiiivaska <iiiivaska@yandex.ru>
2022-02-10 13:30:18 +0300

    Initial Commit

---
+++ Shared/CodeHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeHistoryApp.swift
+//  Shared
+//
+//  Created by Василий Буланов on 2/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CodeHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
